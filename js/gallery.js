b28341b6aca7592a78c08eed97f5730906ff5933
MarvinSangco <karlkenn1012@gmail.com>
2025-03-05 00:19:37 +0800

    Add files via upload

--- js/gallery.js
+++ js/gallery.js
@@ -1,5 +1,19 @@
 /*Name this external file gallery.js*/
 
+window.onload = function() {
+    let images = document.querySelectorAll(".preview");
+
+    images.forEach((img) => {
+        img.setAttribute("tabindex", "0"); // Add tabindex dynamically
+        img.addEventListener("mouseover", function() { upDate(this); });
+        img.addEventListener("mouseout", function() { unDo(); });
+        img.addEventListener("focus", function() { upDate(this); });
+        img.addEventListener("blur", function() { unDo(); });
+    });
+
+    console.log("Event listeners added to all images");
+};
+
 function upDate(previewPic){
     let mainDiv = document.getElementById("image");
     mainDiv.style.backgroundImage = "url('" + previewPic.src + "')";
